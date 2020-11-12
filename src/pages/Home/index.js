7c33f74028325e3abdb0e533becfd884f066b72b
rassacnivek <kevin95cassar@gmail.com>
2020-11-12 18:46:35 +0100

    fix: video height

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div className="home">
       <div className="home__wrapper">
         <div>
-          <ReactPlayer controls width="100vw" url="https://www.youtube.com/watch?v=j2gOU4o1jsQ&ab_channel=CorpseHusband" />
+          <ReactPlayer controls width="100vw" height="38vh" url="https://www.youtube.com/watch?v=j2gOU4o1jsQ&ab_channel=CorpseHusband" />
         </div>
         <Chat />
       </div>
