e5e6995b09659bc2f68de9d5ebf4247a5eeae1f2
rassacnivek <kevin95cassar@gmail.com>
2020-11-17 23:53:58 +0100

    add: styled-components in PageNotFound

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,26 +1,58 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import img from '../../assets/404.png'
-import './pagenotfound.scss';
 
 const PageNotFound = () => {
+  const ErrorContainer = styled.div`
+    height: 80vh;
+    width: 80vw;
+    padding: 10vh 10vw;
+  `
+  const ErrorImg = styled.div`
+    width: 80vw;
+    height: 36vh;
+  `
+  const Img = styled.img`
+    width: 100%;
+    max-height: 100%;
+  `
+  const ErrorText = styled.div`
+    padding: 10vh 0;
+    text-align: center;
+  `
+  const ErrorLink = styled.div`
+    width: 135px;
+    background-color: #9147ff;
+    border: 1px solid #9147ff;
+    border-radius: 5px;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+  `
+  const StyledLink = styled(Link)`
+    color: white;
+    padding: 6px 0px;
+  `
   return (
-    <div className="error">
-      <div className="error__img">
-        <img src={img} alt="404" />
-      </div>
-      <div className="error__text">
+    <ErrorContainer className="error">
+      <ErrorImg>
+        <Img src={img} alt="404" />
+      </ErrorImg>
+      <ErrorText>
         <div>
           I think that's not the page
         </div>
         <div>
           you were looking for..
         </div>
-      </div>
-      <div className="error__button">
-        <Link className="error__button-link" to="/">Let's go Home</Link>
-      </div>
-    </div >
+      </ErrorText>
+      <ErrorLink>
+        <StyledLink to="/">Let's go Home</StyledLink>
+      </ErrorLink>
+    </ErrorContainer>
   )
 }
 
