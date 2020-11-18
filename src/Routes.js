a415fd6bffb3ed73e03a11948beaf79499c9074a
rassacnivek <kevin95cassar@gmail.com>
2020-11-18 01:03:35 +0100

    add: PWA

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Router, withRouter, Switch } from "react-router-dom";
-import Home from './pages/Home';
-import PageNotFound from "./pages/PageNotFound";
 import history from "./utils/history";
 import './App.css';
+
+const Home = lazy(() => import('./pages/Home'))
+const PageNotFound = lazy(() => import('./pages/PageNotFound'))
 
 const Routes = () => {
   return (
     <Router history={history}>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        </div>
+      </Suspense>
     </Router >
   );
 };
